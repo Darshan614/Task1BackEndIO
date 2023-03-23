0ed0913b67e229b80fe967fe241c876a1f724f9b
Darshan614 <jaindarshan614@gmail.com>
2023-03-23 11:13:46 +0530

    cart and product done

--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const productController = require("../controllers/products");
 const isAuth = require("../middleware/is-auth");
 
-router.get("/products/:page", productController.products);
-router.get("/productCount",productController.productCount);
+router.get("/products/:page", isAuth, productController.products);
+router.get("/productCount", productController.productCount);
 router.post("/productInfo", productController.productInfo);
-router.post("/cartData", productController.cartData);
+router.post("/cartData", isAuth, productController.cartData);
 router.post("/similarProducts", productController.similarProducts);
 
 module.exports = router;
